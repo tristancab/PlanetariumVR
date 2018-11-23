da07f7bbc18ef4a9f3ebf687844caad197978283
tristancab <tristan.cabantous@gmail.com>
2018-11-23 17:49:23 +0100

    Création aléatoire d'étoiles

---
+++ Assets/Scripts/Instantiate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour {
+    public GameObject star;
+    public int nbStars;
+    public int distanceFromOrigin;
+	// Use this for initialization
+	void Start () {
+        Initialise();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Initialise()
+    {
+        star = GameObject.Find("Star");
+
+        for (int i = 0; i < nbStars; i++)
+        {
+            int phi = Random.Range(-90, 90);
+            int teta = Random.Range(-180, 180);
+            float x = distanceFromOrigin * Mathf.Sin(phi) * Mathf.Cos(teta);
+            float z = distanceFromOrigin * Mathf.Cos(phi);
+            float y = distanceFromOrigin * Mathf.Sin(phi) * Mathf.Sin(teta);
+            if(y < 0)
+            {
+                y = -y;
+            }
+            Instantiate(star, new Vector3(x, y, z), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour {
+    public string name;
+    public Vector3 coord;
+    public float brightness;
+    public string consstellationName;
+    public Star neighboors;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Nom : " + name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
